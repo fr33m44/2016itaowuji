dd4ed7de56acf917c34465333b9312ea6c158d72
sh1t h4pp3ns <228787+fr33m44@users.noreply.github.com>
2016-09-10 03:31:54 +0000

    首页轮播图修改为显示5张 jquery superslide effect属性 left修改为leftLoop 左循环轮播 加入QQ客服链接

--- data/flashdata/redfocus/data.js
+++ data/flashdata/redfocus/data.js
@@ -1,13 +1,19 @@
-imgUrl1="data/afficheimg/20160823sdysui.jpg";
+imgUrl1="data/afficheimg/20160910uilzvz.jpg";
 imgtext1="";
-imgLink1=escape("http://www.itaowuji.com/category.php?id=183");
-imgUrl2="data/afficheimg/20160813dybfqg.jpg";
+imgLink1=escape("http://保暖衣");
+imgUrl2="data/afficheimg/20160910cdechf.jpg";
 imgtext2="";
-imgLink2=escape("http://");
-imgUrl3="data/afficheimg/20160813xzwhqt.jpg";
+imgLink2=escape("http://打底裤");
+imgUrl3="data/afficheimg/20160910usleox.jpg";
 imgtext3="";
-imgLink3=escape("http://www.itaowuji.com/category.php?id=183");
+imgLink3=escape("http://首页");
+imgUrl4="data/afficheimg/20160910cjfxbq.jpg";
+imgtext4="";
+imgLink4=escape("http://内衣");
+imgUrl5="data/afficheimg/20160910sucqos.jpg";
+imgtext5="";
+imgLink5=escape("http://居家服");
 
-var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3;
-var links=imgLink1+"|"+imgLink2+"|"+imgLink3;
-var texts=imgtext1+"|"+imgtext2+"|"+imgtext3;
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3+"|"+imgUrl4+"|"+imgUrl5;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3+"|"+imgLink4+"|"+imgLink5;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3+"|"+imgtext4+"|"+imgtext5;
