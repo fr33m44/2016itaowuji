bfa290e8de3cafc0b4ea81d784e226976deeaa96
sh1t h4pp3ns <228787+fr33m44@users.noreply.github.com>
2016-09-13 06:43:54 +0000

    后台kindeditor上传限制1M->5M 调试微信支付

--- wx_native_callback.php
+++ wx_native_callback.php
@@ -3,6 +3,13 @@ define('IN_ECS', true);
 require(dirname(__FILE__) . '/includes/init.php');
 require(ROOT_PATH . 'includes/lib_payment.php');
 require_once(ROOT_PATH .'includes/modules/payment/wx_new_qrcode.php');
+
+$fp =  fopen(ROOT_PATH.'/data/wx_new_log.txt','a+');
+fwrite($fp,'-----------'.local_date('Y-m-d H:i:s').'-----------------');
+fwrite($fp,'in callback file');
+fclose($fp);
+
 $payment = new wx_new_qrcode();
+print_r($payment);
 $payment->respond();
 exit;
