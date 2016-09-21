eb28d15c7e2065b34dcf15a25f94befba59ec5f6
hjq <hjq@5c7bab8f-1a59-594b-8cdb-7e0f5bd4832b>
2016-09-21 05:46:07 +0000

    支付宝支付后，没有跳转到itwj的情况下关闭页面网站无法判断订单已经是已付款状态 fix:支付宝异步回调，本地应该是无法测试的。只能上传到公网，这样支付宝服务器才能访问notify_url.php

--- trunk/includes/modules/payment/alipay/notify_url.php
+++ trunk/includes/modules/payment/alipay/notify_url.php
@@ -14,7 +14,12 @@
  * 该页面调试工具请使用写文本函数logResult，该函数已被默认关闭，见alipay_notify_class.php中的函数verifyNotify
  * 如果没有收到该页面返回的 success 信息，支付宝会在24小时内按一定的时间策略重发通知
  */
-
+$fp = fopen("hjq.txt","a+");
+	flock($fp, LOCK_EX) ;
+	fwrite($fp,"执行日期：".strftime("%Y%m%d%H%M%S",time())."\n"."hjq"."\n");
+	flock($fp, LOCK_UN);
+	fclose($fp);
+	
 require_once("alipay.config.php");
 require_once("lib/alipay_notify.class.php");
 
@@ -22,7 +27,8 @@ require_once("lib/alipay_notify.class.php");
 $alipayNotify = new AlipayNotify($alipay_config);
 $verify_result = $alipayNotify->verifyNotify();
 
-if($verify_result) {//验证成功
+if($verify_result) 
+{	//验证成功
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//请在这里加上商户的业务逻辑程序代
 
@@ -40,43 +46,44 @@ if($verify_result) {//验证成功
 	//交易状态
 	$trade_status = $_POST['trade_status'];
 
+	//交易金额
+	
+	$total_fee = $_POST['total_fee'];
+	
+	//返回商户订单号
+	$order_sn = str_replace($_POST['subject'], '', $_POST['out_trade_no']);
+	$order_sn = trim(addslashes($order_sn));
+	$pay_log_id = get_order_id_by_sn($order_sn);
+	$order_id = get_order_id_by_sn1($order_sn);
 
-    if($_POST['trade_status'] == 'TRADE_FINISHED') {
-		//判断该笔订单是否在商户网站中已经做过处理
-			//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-			//请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
-			//如果有做过处理，不执行商户的业务程序
-				
-		//注意：
-		//退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
-
-        //调试用，写文本函数记录程序运行情况是否正常
-        //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+	logResult($_POST);
+    if($_POST['trade_status'] == 'TRADE_FINISHED' || $_POST['trade_status'] == 'TRADE_SUCCESS')
+	{
+        /* 检查支付的金额是否相符 */
+		if (check_money($pay_log_id, $total_fee))
+        {
+			//echo 'check money passed';
+			order_paid($pay_log_id, 2);
+        }
+		else
+		{
+			echo '支付金额不正确！';
+		}
     }
-    else if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
-		//判断该笔订单是否在商户网站中已经做过处理
-			//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-			//请务必判断请求时的total_fee、seller_id与通知时获取的total_fee、seller_id为一致的
-			//如果有做过处理，不执行商户的业务程序
-				
-		//注意：
-		//付款完成后，支付宝系统发送该交易状态通知
-
-        //调试用，写文本函数记录程序运行情况是否正常
-        //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
-    }
-
-	//——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
-        
+    else
+	{
+		echo "fail";
+	   
+    } 
 	echo "success";		//请不要修改或删除
 	
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
-else {
+else
+{
     //验证失败
     echo "fail";
-
-    //调试用，写文本函数记录程序运行情况是否正常
-    //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
 }
+
+
 ?>
