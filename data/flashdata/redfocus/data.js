4d379ea181834f34bfc4d0656a9a8ed7c14c1bc9
sh1t h4pp3ns <228787+fr33m44@users.noreply.github.com>
2016-10-10 01:55:59 +0000

    mobile 首页模板修改

--- data/flashdata/redfocus/data.js
+++ data/flashdata/redfocus/data.js
@@ -1,18 +1,18 @@
-imgUrl1="data/afficheimg/20160923tyffli.jpg";
+imgUrl1="data/afficheimg/20161009hqdjdg.jpg";
 imgtext1="";
-imgLink1=escape("http://itwj/user.php?act=register");
-imgUrl2="data/afficheimg/20160923wsdplb.jpg";
+imgLink1=escape("http://www.itaowuji.com/user.php?act=register");
+imgUrl2="data/afficheimg/20161009pdsbib.jpg";
 imgtext2="";
-imgLink2=escape("http://itwj/category.php?id=160");
-imgUrl3="data/afficheimg/20160923nhlxpb.jpg";
+imgLink2=escape("http://www.itaowuji.com/category.php?id=160");
+imgUrl3="data/afficheimg/20161009qylyuu.jpg";
 imgtext3="";
-imgLink3=escape("http://itwj/category.php?id=162");
-imgUrl4="data/afficheimg/20160923mumvjv.jpg";
+imgLink3=escape("http://www.itaowuji.com/category.php?id=162");
+imgUrl4="data/afficheimg/20161009xnerig.jpg";
 imgtext4="";
-imgLink4=escape("http://itwj/category.php?id=165");
-imgUrl5="data/afficheimg/20160923uucozw.jpg";
+imgLink4=escape("http://www.itaowuji.com/category.php?id=165");
+imgUrl5="data/afficheimg/20161009gsvnqg.jpg";
 imgtext5="";
-imgLink5=escape("http://itwj/category.php?id=167");
+imgLink5=escape("http://www.itaowuji.com/category.php?id=167");
 
 var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3+"|"+imgUrl4+"|"+imgUrl5;
 var links=imgLink1+"|"+imgLink2+"|"+imgLink3+"|"+imgLink4+"|"+imgLink5;
