a84b4ba062a1cde3d562f0bd8f89aa5d4f582b19
sh1t h4pp3ns <228787+fr33m44@users.noreply.github.com>
2016-10-12 08:26:58 +0000

    utils.js update

--- mobile/data/static/js/utils.js
+++ mobile/data/static/js/utils.js
@@ -77,6 +77,11 @@ Utils.isTel = function ( tel )
 
   return reg.test( tel );
 }
+Utils.isMobile = function(mobile)
+{
+	var reg = /^1[34578]\d{9}$/;
+	return reg.test(mobile);
+}
 
 Utils.fixEvent = function(e)
 {
