69493a2fc100274c46ffd32b7ccbbf9a165dfbc2
hjq <hjq@5c7bab8f-1a59-594b-8cdb-7e0f5bd4832b>
2016-11-12 08:58:14 +0000

    加入mobile下的新增文件

---
+++ trunk/admin/wxch_lg.php
@@ -0,0 +1,8 @@
+<?php
+define('IN_ECS', true);
+error_reporting(0);
+$wxch_lang['copyright'] = 'Copyright &copy; 2016 itaowuji.com';
+$wxch_lang['cp_home'] = '微信设置';
+$wxch_lang['kefu'] = '';
+$wxch_lang['config'] = '微信设置';
+?>
